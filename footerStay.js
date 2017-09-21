ef4859c94640200ebd98eba12e0b35137e950fa4
Cobber <codecobber@gmail.com>
2017-09-21 11:45:53 +0100

    Made call to function when page loads

--- footerStay.js
+++ footerStay.js
@@ -1,19 +1,23 @@
  <script>
-
-      window.onresize = function(event) {
+   
+   function footerResize(){
         var ht = window.innerHeight;
         var ft = document.getElementById('footer');
         var ftH = ft.offsetHeight;
         var menu = document.getElementById('main');
         var menuH = menu.offsetHeight;
         
-        
-        
         var readyHeight = (ht-ftH-menuH)+"px";
         var contArea = document.getElementById('contentBCK');
         contArea.style.height = readyHeight;
+    }
+
+        
+      window.onresize = function(event) {
+        footerResize();
       };
 
+      footerResize();
       
 
     </script>
